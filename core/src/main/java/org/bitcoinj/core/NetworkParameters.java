5fc262f95cbae2b6166a8f0c56806e3bf36d38b0
laudney <ren@reddcoin.com>
2014-11-16 17:19:52 +0000

    Set the correct input for genesis tx.

--- core/src/main/java/org/bitcoinj/core/NetworkParameters.java
+++ core/src/main/java/org/bitcoinj/core/NetworkParameters.java
@@ -109,7 +109,7 @@ public abstract class NetworkParameters implements Serializable {
             //
             //   "January 21st 2014 was such a nice day..."
             byte[] bytes = Utils.HEX.decode
-                    ("04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73");
+                    ("04ffff001d0104284a616e75617279203231737420323031342077617320737563682061206e696365206461792e2e2e");
             t.addInput(new TransactionInput(n, t, bytes));
             ByteArrayOutputStream scriptPubKeyBytes = new ByteArrayOutputStream();
             Script.writeBytes(scriptPubKeyBytes, Utils.HEX.decode
